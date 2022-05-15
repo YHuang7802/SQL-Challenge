9b9351edc4bba968f92478c405a1a0f10ff6a0b9
unknown <yhuang7802@gmail.com>
2022-05-15 02:04:27 -0400

    hw checkpoint

--- EmployeeSQL/table_queries.sql
+++ EmployeeSQL/table_queries.sql
@@ -28,3 +28,35 @@ on "Department".dept_no = "Dept_emp".dept_no
 join "Employees"
 on "Dept_emp".emp_no = "Employees".emp_no;
 
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+select "Employees".first_name, "Employees".last_name, "Employees".sex
+from "Employees"
+where first_name like 'Hercules' and last_name like 'B%';
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+select "Employees".emp_no, "Employees".last_name, "Employees".first_name, "Department".dept_name
+from "Dept_emp"
+join "Department"
+on "Department".dept_no = "Dept_emp".dept_no
+join "Employees"
+on "Dept_emp".emp_no = "Employees".emp_no
+where dept_name like 'Sales';
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+select "Employees".emp_no, "Employees".last_name, "Employees".first_name, "Department".dept_name
+from "Dept_emp"
+join "Department"
+on "Department".dept_no = "Dept_emp".dept_no
+join "Employees"
+on "Dept_emp".emp_no = "Employees".emp_no
+where dept_name like 'Sales' or dept_name like 'Development';
+
+--List the frequency count of employee last names (i.e., how many employees share each last name) in descending order.
+select last_name, count(last_name) as frequency 
+from "Employees" group by last_name order by count(last_name) desc
+
+
+
+
+
+
--- table_queries.sql
+++
@@ -1,5 +0,0 @@
-select "Employees".emp_no,"Salaries".emp_no, "Employees".last_name,"Employees".first_name, "Employees".sex,  "Salaries".salary   
-from "Salaries" 
-right join "Employees" 
-on "Employees".emp_no = "Salaries".emp_no; 
-
